c2f9b1a4a121e5b342ac1593ecc533f9a8a384b6
Eduardo Dutra <eduardo.dutra@habibs.com.br>
2018-08-30 23:23:22 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Cassandra;
+
+namespace cassandra_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cluster = Cluster.Builder()
+                     .AddContactPoints("aws.overbug.com.br")
+                     .Build();
+            // Connect to the nodes using a keyspace
+            var session = cluster.Connect("test");
+            // Execute a query on a connection synchronously
+            var rs = session.Execute("SELECT * FROM emp;");
+            // Iterate through the RowSet
+            foreach (var row in rs)
+            {
+            var value = row.GetValue<int>("sample_int_column");
+            // Do something with the value
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
